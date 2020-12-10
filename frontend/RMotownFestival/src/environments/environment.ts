b4de97dbd451809bf2c6ce19829e2af19bd9c0f7
Georges Grajchen <Georges.Grajchen@realdolmen.com>
2020-12-10 13:04:06 +0100

    appsettings

--- frontend/RMotownFestival/src/environments/environment.ts
+++ frontend/RMotownFestival/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   festivalName: "RMotownFestival - DEV",
-  apiBaseUrl: "https://localhost:44395/api/"
+  apiBaseUrl: "rmotownfestivalapigg.azurewebsites.net/api"
 };
 
 /*
